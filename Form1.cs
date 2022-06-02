18df52e7f4298c0fb335236723dbbd25935d8bd8
Ram Ymac E. Llena <104246670+RamYmacLlena@users.noreply.github.com>
2022-06-02 22:01:31 +0800

    Trying out my first condition

--- Form1.cs
+++ Form1.cs
@@ -7,9 +7,22 @@ namespace Play_Tic_Tac_Toe_game
             InitializeComponent();
         }
 
+        int Count = 1;
+
         private void AllButton(object sender, EventArgs e)
         {
-
+            Button b = (Button)sender;
+            b.Text = (Count % 2 == 0) ? "X" : "0";
+            b.Enabled = false;
+            check();
+            Count++;
+        }
+        public void check()
+        {
+            if (SLOT1_TOPLEFT.Text == "0" && SLOT2_TOPMIDDLE.Text == "0" && SLOT3_TOPRIGHT.Text == "0")
+            {
+                MessageBox.Show("Player 1 won the game");
+            }
         }
     }
 }
