5eb5089f06923667de5de43cf40eb26ad11137dd
Ram Ymac E. Llena <104246670+RamYmacLlena@users.noreply.github.com>
2022-06-03 18:43:26 +0800

    Added Label for names Player 1 and Player 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,8 @@
             this.SLOT7_BOTTOMLEFT = new System.Windows.Forms.Button();
             this.SLOT8_BOTTOMMIDDLE = new System.Windows.Forms.Button();
             this.SLOT9_BOTTOMRIGHT = new System.Windows.Forms.Button();
+            this.PLAYER1 = new System.Windows.Forms.Label();
+            this.PLAYER2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // SLOT1_TOPLEFT
@@ -120,11 +122,36 @@
             this.SLOT9_BOTTOMRIGHT.UseVisualStyleBackColor = true;
             this.SLOT9_BOTTOMRIGHT.Click += new System.EventHandler(this.AllButton);
             // 
+            // PLAYER1
+            // 
+            this.PLAYER1.AutoSize = true;
+            this.PLAYER1.BackColor = System.Drawing.Color.Aqua;
+            this.PLAYER1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PLAYER1.Location = new System.Drawing.Point(546, 12);
+            this.PLAYER1.Name = "PLAYER1";
+            this.PLAYER1.Size = new System.Drawing.Size(187, 54);
+            this.PLAYER1.TabIndex = 9;
+            this.PLAYER1.Text = "PLAYER 1";
+            // 
+            // PLAYER2
+            // 
+            this.PLAYER2.AutoSize = true;
+            this.PLAYER2.BackColor = System.Drawing.Color.Aqua;
+            this.PLAYER2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PLAYER2.Location = new System.Drawing.Point(546, 264);
+            this.PLAYER2.Name = "PLAYER2";
+            this.PLAYER2.Size = new System.Drawing.Size(187, 54);
+            this.PLAYER2.TabIndex = 10;
+            this.PLAYER2.Text = "PLAYER 2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 574);
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 491);
+            this.Controls.Add(this.PLAYER2);
+            this.Controls.Add(this.PLAYER1);
             this.Controls.Add(this.SLOT9_BOTTOMRIGHT);
             this.Controls.Add(this.SLOT8_BOTTOMMIDDLE);
             this.Controls.Add(this.SLOT7_BOTTOMLEFT);
@@ -137,6 +164,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -151,5 +179,7 @@
         private Button SLOT7_BOTTOMLEFT;
         private Button SLOT8_BOTTOMMIDDLE;
         private Button SLOT9_BOTTOMRIGHT;
+        private Label PLAYER1;
+        private Label PLAYER2;
     }
 }
