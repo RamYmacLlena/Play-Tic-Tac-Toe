3afcd79b419974f60a1b3a56304dc5f03849f1d2
Ram Ymac E. Llena <104246670+RamYmacLlena@users.noreply.github.com>
2022-06-03 18:20:10 +0800

    modified function for the form to reset when there is a winner

--- Form1.cs
+++ Form1.cs
@@ -20,48 +20,56 @@ namespace Play_Tic_Tac_Toe_game
         public void check()
         {
             bool Tie = true;
-            //start here for player 1 winning conditions
+            //start here for player 2 winning conditions
             if (SLOT1_TOPLEFT.Text == "0" && SLOT2_TOPMIDDLE.Text == "0" && SLOT3_TOPRIGHT.Text == "0")
             {
                 Tie = false;
                 MessageBox.Show("Player 2 won the game!");
+                Winner("Player 2 won the game!");
             }
             if (SLOT1_TOPLEFT.Text == "0" && SLOT4_MIDDLELEFT.Text == "0" && SLOT7_BOTTOMLEFT.Text == "0")
             {
                 Tie = false;
                 MessageBox.Show("Player 2 won the game!");
+                Winner("Player 2 won the game!");
             }
             if (SLOT1_TOPLEFT.Text == "0" && SLOT5_CENTER.Text == "0" && SLOT9_BOTTOMRIGHT.Text == "0")
             {
                 Tie = false;
                 MessageBox.Show("Player 2 won the game!");
+                Winner("Player 2 won the game!");
             }
             if (SLOT2_TOPMIDDLE.Text == "0" && SLOT5_CENTER.Text == "0" && SLOT8_BOTTOMMIDDLE.Text == "0")
             {
                 Tie = false;
                 MessageBox.Show("Player 2 won the game!");
+                Winner("Player 2 won the game!");
             }
             if (SLOT3_TOPRIGHT.Text == "0" && SLOT6_MIDDLERIGHT.Text == "0" && SLOT9_BOTTOMRIGHT.Text == "0")
             {
                 Tie = false;
                 MessageBox.Show("Player 2 won the game!");
+                Winner("Player 2 won the game!");
             }
             if (SLOT4_MIDDLELEFT.Text == "0" && SLOT5_CENTER.Text == "0" && SLOT6_MIDDLERIGHT.Text == "0")
             {
                 Tie = false;
                 MessageBox.Show("Player 2 won the game!");
+                Winner("Player 2 won the game!");
             }
             if (SLOT7_BOTTOMLEFT.Text == "0" && SLOT8_BOTTOMMIDDLE.Text == "0" && SLOT9_BOTTOMRIGHT.Text == "0")
             {
                 Tie = false;
                 MessageBox.Show("Player 2 won the game!");
+                Winner("Player 2 won the game!");
             }
             if (SLOT3_TOPRIGHT.Text == "0" && SLOT5_CENTER.Text == "0" && SLOT7_BOTTOMLEFT.Text == "0")
             {
                 Tie = false;
                 MessageBox.Show("Player 2 won the game!");
+                Winner("Player 2 won the game!");
             }
-            //start her for player 2 winning conditions
+            //start her for player 1 winning conditions
             if (SLOT1_TOPLEFT.Text == "X" && SLOT2_TOPMIDDLE.Text == "X" && SLOT3_TOPRIGHT.Text == "X")
             {
                 Tie = false;
@@ -72,36 +80,43 @@ namespace Play_Tic_Tac_Toe_game
             {
                 Tie = false;
                 MessageBox.Show("Player 1 won the game!");
+                Winner("Player 1 won the game!");
             }
             if (SLOT1_TOPLEFT.Text == "X" && SLOT5_CENTER.Text == "X" && SLOT9_BOTTOMRIGHT.Text == "X")
             {
                 Tie = false;
                 MessageBox.Show("Player 1 won the game!");
+                Winner("Player 1 won the game!");
             }
             if (SLOT2_TOPMIDDLE.Text == "X" && SLOT5_CENTER.Text == "X" && SLOT8_BOTTOMMIDDLE.Text == "X")
             {
                 Tie = false;
                 MessageBox.Show("Player 1 won the game!");
+                Winner("Player 1 won the game!");
             }
             if (SLOT3_TOPRIGHT.Text == "X" && SLOT6_MIDDLERIGHT.Text == "X" && SLOT9_BOTTOMRIGHT.Text == "X")
             {
                 Tie = false;
                 MessageBox.Show("Player 1 won the game!");
+                Winner("Player 1 won the game!");
             }
             if (SLOT4_MIDDLELEFT.Text == "X" && SLOT5_CENTER.Text == "X" && SLOT6_MIDDLERIGHT.Text == "X")
             {
                 Tie = false;
                 MessageBox.Show("Player 1 won the game!");
+                Winner("Player 1 won the game!");
             }
             if (SLOT7_BOTTOMLEFT.Text == "X" && SLOT8_BOTTOMMIDDLE.Text == "X" && SLOT9_BOTTOMRIGHT.Text == "X")
             {
                 Tie = false;
                 MessageBox.Show("Player 1 won the game!");
+                Winner("Player 1 won the game!");
             }
             if (SLOT3_TOPRIGHT.Text == "X" && SLOT5_CENTER.Text == "X" && SLOT7_BOTTOMLEFT.Text == "X")
             {
                 Tie = false;
                 MessageBox.Show("Player 1 won the game!");
+                Winner("Player 1 won the game!");
             }
 
             else if (Count == 9 && Tie == true)
@@ -152,7 +167,7 @@ namespace Play_Tic_Tac_Toe_game
             }
             else if (text == "Player 2 won the game!")
             {
-                MessageBox.Show("Congratulations Player 2");
+                MessageBox.Show("Congratulations Player 2!");
             }
             string message = "DO YOU WANT TO PLAY AGAIN?";
             string title = "THE GAME HAS ENDED!";
